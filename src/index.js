bc1e2af7d4c9fb88eef9ab60dd676e30b4849764
Paul Henschel <drcmda@gmail.com>
2021-04-16 15:04:12 +0200

    update test

--- src/index.js
+++ src/index.js
@@ -1,113 +1,129 @@
-import ReactDOM from 'react-dom'
-import React, { useRef, useState, useEffect } from 'react'
-import { BoxBufferGeometry, MeshNormalMaterial } from 'three'
-import { Canvas, Dom, useFrame, useThree } from 'react-three-fiber'
-import { Controls, useControl } from 'react-three-gui'
-import { unstable_LowPriority as low, unstable_runWithPriority as run } from 'scheduler'
-import './styles.css'
+import * as THREE from "three";
+import ReactDOM from "react-dom";
+import React, { Suspense, useRef, useState, useEffect, useMemo, useLayoutEffect } from "react";
+import { Canvas, useFrame, useThree, useLoader } from "@react-three/fiber";
+import { useControls } from "leva";
+import { Stats } from "@react-three/drei";
+import "./styles.css";
 
-const SLOWDOWN = 1
-const ROW = 20
-const BLOCK_AMOUNT = 600
-const SPIKE_AMOUNT = 1000
-const geom = new BoxBufferGeometry(1, 1, 1)
-const matr = new MeshNormalMaterial()
-const rpi = () => Math.random() * Math.PI
+const SLOWDOWN = 1;
+const ROW = 20;
+const BLOCK_AMOUNT = 500;
+const SEGMENTS = 80;
+const geom = new THREE.PlaneGeometry(1, 1);
+const vec = new THREE.Color();
 
-function Block({ change, ...props }) {
-  const [color, set] = useState(0)
+const colors = ["#A2CCB6", "#FCEEB5", "#EE786E", "#e0feff", "lightpink", "lightblue"];
+const chars = `!"§$%&/()=?*#<>-_.:,;+0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`;
 
-  if (color > 0) {
-    // Artificial slowdown ...
-    const e = performance.now() + SLOWDOWN
-    while (performance.now() < e) {}
-  }
+function Block({ distribute, change, ...props }) {
+  const [color, setColor] = useState(0);
+  const [char, setChar] = useState(chars[Math.floor(Math.random() * chars.length)]);
 
   useEffect(() => {
-    // Set color, signaling low priority to the scheduler
-    // Not even sure if that does anything, it seems to be OK without, too ...
-    if (change) setTimeout(() => run(low, () => set(Math.round(Math.random() * 0xffffff))), Math.random() * 1000)
-  }, [change])
+    function set() {
+      setChar(chars[Math.floor(Math.random() * chars.length)]);
+      setColor(colors[Math.floor(Math.random() * colors.length)]);
+    }
+    if (distribute) setTimeout(set, Math.random() * 1000);
+    else set();
+  }, [change]);
+
+  const mat = useRef();
+  useFrame((state, delta) => mat.current.color.lerp(vec.set("white"), 0.01));
+  useLayoutEffect(() => void mat.current.color.set("red"), [color]);
 
   return (
     <mesh {...props} geometry={geom}>
-      <meshBasicMaterial attach="material" color={color} />
+      <meshBasicMaterial ref={mat} />
+      {color !== 0 && <Text size={5}>{char}</Text>}
     </mesh>
-  )
+  );
 }
 
-function Blocks() {
-  const [changeBlocks, set] = useState(false)
+function Blocks({ distribute }) {
+  const [changeBlocks, set] = useState(false);
   useEffect(() => {
-    const handler = setInterval(() => set(state => !state), 2000)
-    return () => clearInterval(handler)
-  })
+    const handler = setInterval(() => set((state) => !state), 2000);
+    return () => clearInterval(handler);
+  });
 
-  const { viewport } = useThree()
-  const width = viewport.width / 100
-  const size = width / ROW
+  const { viewport } = useThree();
+  const width = viewport.width;
+  const size = width / ROW;
   return new Array(BLOCK_AMOUNT).fill().map((_, i) => {
-    const left = -viewport.width / 100 / 2 + size / 2
-    const top = viewport.height / 100 / 2 - size / 2
-    const x = (i % ROW) * size
-    const y = Math.floor(i / ROW) * -size
-    return <Block key={i} change={changeBlocks} scale={[size, size, size]} position={[left + x, top + y, 0]} />
-  })
-}
-
-function Fps() {
-  let ref = useRef()
-  let decimalPlacesRatio = Math.pow(10, 0)
-  let timeMeasurements = []
-  let fps = 0
-  let last = 0
-  useFrame(() => {
-    timeMeasurements.push(performance.now())
-    const msPassed = timeMeasurements[timeMeasurements.length - 1] - timeMeasurements[0]
-    if (msPassed >= 0.25 * 1000) {
-      fps = Math.round((timeMeasurements.length / msPassed) * 1000 * decimalPlacesRatio) / decimalPlacesRatio
-      timeMeasurements = []
-    }
-    if (fps !== last)
-      ref.current.innerText =
-        `${SPIKE_AMOUNT} spikes\n${BLOCK_AMOUNT} blocks\n${BLOCK_AMOUNT * SLOWDOWN}ms potential load\nfps avg ` + fps
-    last = fps
-  })
-  return <Dom className="fps" center ref={ref} />
-}
-
-function Box() {
-  let t = 0
-  const mesh = useRef()
-  const [coords] = useState(() => [rpi(), rpi(), rpi()])
-  useFrame(({ clock }) => mesh.current.rotation.set(coords[0] + (t += 0.01), coords[1] + t, coords[2] + t))
-  return <mesh ref={mesh} geometry={geom} material={matr} scale={[2, 2, 2]} />
-}
-
-function AnimatedSpikes() {
-  return new Array(SPIKE_AMOUNT).fill().map((_, i) => <Box key={i} />)
+    const left = -viewport.width / 2 + size / 2;
+    const top = viewport.height / 2 - size / 2;
+    const x = (i % ROW) * size;
+    const y = Math.floor(i / ROW) * -size;
+    return (
+      <Block
+        key={i}
+        distribute={distribute}
+        change={changeBlocks}
+        scale={[size, size, size]}
+        position={[left + x, top + y, 0]}
+      />
+    );
+  });
 }
 
 function Dolly() {
-  const { clock, camera } = useThree()
-  useFrame(() => camera.updateProjectionMatrix(void (camera.zoom = 130 + Math.sin(clock.getElapsedTime() * 3) * 30)))
-  return null
+  const { clock, camera } = useThree();
+  useFrame(() => camera.updateProjectionMatrix(void (camera.zoom = 130 + Math.sin(clock.getElapsedTime() * 3) * 30)));
+  return null;
+}
+
+function Text({ children, vAlign = "center", hAlign = "center", size = 1, color = "#000000", ...props }) {
+  const font = useLoader(THREE.FontLoader, "/Inter UI_Bold.json");
+  const config = useMemo(
+    () => ({
+      font,
+      size: 1,
+      height: 0.5,
+      curveSegments: SEGMENTS,
+      bevelEnabled: false,
+    }),
+    [font, SEGMENTS],
+  );
+  const mesh = useRef();
+  useLayoutEffect(() => {
+    const size = new THREE.Vector3();
+    mesh.current.geometry.computeBoundingBox();
+    mesh.current.geometry.boundingBox.getSize(size);
+    mesh.current.position.x = hAlign === "center" ? -size.x / 2 : hAlign === "right" ? 0 : -size.x;
+    mesh.current.position.y = vAlign === "center" ? -size.y / 2 : vAlign === "top" ? 0 : -size.y;
+  }, [children]);
+
+  const geom = useMemo(() => new THREE.TextGeometry(children, config), [children, config]);
+
+  return (
+    <group {...props} scale={[0.1 * size, 0.1 * size, 0.1]}>
+      <mesh ref={mesh} geometry={geom} >
+        <meshStandardMaterial color="#202020" depthTest={false}/>
+      </mesh>
+    </group>
+  );
 }
 
 function App() {
-  const root = useControl('React', { type: 'select', group: 'Performance', items: ['Concurrent', 'Legacy'] })
-  const concurrent = root === 'Concurrent'
+  const { concurrent, distribute } = useControls({ concurrent: true, distribute: false });
   return (
     <>
-      <Canvas concurrent={concurrent} key={root} orthographic camera={{ zoom: 100 }} style={{ background: '#272737' }}>
-        <Fps />
-        <Blocks />
-        <AnimatedSpikes />
+      <Canvas
+        key={concurrent}
+        orthographic
+        mode={concurrent ? "concurrent" : "blocking"}
+        camera={{ position: [0, 0, 100], zoom: 100 }}
+        style={{ background: "#272737" }}>
+        <Suspense fallback={null}>
+          <Blocks distribute={distribute} />
+        </Suspense>
         <Dolly />
       </Canvas>
-      <Controls />
+      <Stats className='fps' />
     </>
-  )
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
